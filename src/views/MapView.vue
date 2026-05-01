3f6f8a63a4efeab0721c6ad2224557ade4c9b1a4
meddiecap <postvoormathijs@gmail.com>
2026-05-01 10:15:09 -0700

    formatting

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -7,26 +7,16 @@ const zoom = ref(5)
 const center = ref<[number, number]>([51.5, 10.0]) // centred over Europe
 
 const tileUrl =
-  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+    "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
 const tileAttribution =
-  '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 </script>
 
 <template>
-  <div class="w-full h-dvh">
-    <LMap
-      :zoom="zoom"
-      :center="center"
-      :use-global-leaflet="false"
-      class="w-full h-full"
-    >
-      <LTileLayer
-        :url="tileUrl"
-        :attribution="tileAttribution"
-        layer-type="base"
-        name="CartoDB Dark"
-        :max-zoom="19"
-      />
-    </LMap>
-  </div>
+    <div class="w-full h-dvh">
+        <LMap :zoom="zoom" :center="center" :use-global-leaflet="false" class="w-full h-full">
+            <LTileLayer :url="tileUrl" :attribution="tileAttribution" layer-type="base" name="CartoDB Dark"
+                :max-zoom="19" />
+        </LMap>
+    </div>
 </template>
